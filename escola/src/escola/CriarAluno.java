90c398920c2cd75ee385078d09f2c4a85575db52
Jeff <jefff>
2019-09-13 21:39:44 -0300

    a

--- escola/src/escola/CriarAluno.java
+++ escola/src/escola/CriarAluno.java
@@ -9,7 +9,28 @@ public class CriarAluno {
 		aluno1.idade = 34;
 		aluno1.nota1 = 7.5;
 		aluno1.nota2 = 8.2;
-
+		
+		Aluno aluno2 = new Aluno();
+		aluno2.nome = "Nelson";
+		aluno2.idade = 54;
+		aluno2.nota1 = 6.5;
+		aluno2.nota2 = 9.2;
+		
+		System.out.println("Primeiro Aluno: ");
+		System.out.println("Nome: " + aluno1.nome);
+		System.out.println("Idade: " + aluno1.idade);
+		System.out.println("Nota 1: " + aluno1.nota1);
+		System.out.println("Nota 2: " + aluno1.nota2);
+		
+		System.out.println();
+		
+		System.out.println("Segundo Aluno: ");
+		System.out.println("Nome: " + aluno2.nome);
+		System.out.println("Idade: " + aluno2.idade);
+		System.out.println("Nota 1: " + aluno2.nota1);
+		System.out.println("Nota 2: " + aluno2.nota2);
+		
+		
 	}
 
 }
